5eb1cf819d3312ba9aca16a8a1fb6715b33113a7
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-12 03:05:51 +0530

    Use Wordpress controlled menus for header/footer navigation

--- wordpress/wp-content/themes/fictional-university-theme/functions.php
+++ wordpress/wp-content/themes/fictional-university-theme/functions.php
@@ -16,7 +16,12 @@ function university_files() {
 
 function university_features() {
   add_theme_support('title-tag');
+
+  register_nav_menu('header_menu', 'Header Menu');
+  register_nav_menu('footer_menu_1', 'Footer Menu 1');
+  register_nav_menu('footer_menu_2', 'Footer Menu 2');
 }
+
 
 add_action('wp_enqueue_scripts', 'university_files');
 add_action('after_setup_theme', 'university_features');
