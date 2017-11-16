516b546b71c613938af826e31b5d68960ddb9fb2
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-16 16:45:07 +0530

    Add pagination support to custom query (in past events page)

--- wordpress/wp-content/themes/fictional-university-theme/page-past-events.php
+++ wordpress/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -18,6 +18,7 @@ get_header();
   $today = date('Ymd');
 
   $pastEvents = new WP_Query(array(
+    'paged' => get_query_var('paged', 1),
     'post_type' => 'event',
     'meta_key' => 'event_date',
     'orderby' => 'meta_value_num',
@@ -49,7 +50,9 @@ get_header();
   <?php
   }
 
-  echo paginate_links();
+  echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+  ));
   ?>
 
   </div>
