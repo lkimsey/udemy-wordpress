095838ddd1c66c244574b888e4042ad664cb48ed
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-18 23:44:41 +0530

    Replace page banner HTML in each template with pageBanner function

--- wordpress/wp-content/themes/fictional-university-theme/archive.php
+++ wordpress/wp-content/themes/fictional-university-theme/archive.php
@@ -2,29 +2,11 @@
 
 get_header();
 
+pageBanner(array(
+  'title' => archiveTitle(),
+  'subtitle' => get_the_archive_description()
+));
 ?>
-  <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">
-      <?php
-        if(is_category()) {
-          single_cat_title();
-        }
-        else if(is_author()) {
-          echo 'Post(s) by '; the_author();
-        }
-        else {
-          the_archive_title();
-        }
-      ?>
-    </h1>
-      <div class="page-banner__intro">
-      <p><?php the_archive_description(); ?></p>
-      </div>
-    </div>
-  </div>
-
   <div class="container container--narrow page-section">
   <?php
   while(have_posts()) {
@@ -54,3 +36,15 @@ get_header();
 
 <?php
 get_footer();
+
+
+function archiveTitle() {
+  if(is_category()) {
+    return single_cat_title(null, false);
+  }
+  else if(is_author()) {
+    return'Post(s) by ' .  get_the_author();
+  }
+
+  return get_the_archive_title();
+}
