bba7212ad89989bd17b2541ef9a0981cf22d5b7c
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-25 05:38:46 +0530

    Include program relations (professors) in search result

--- wordpress/wp-content/themes/fictional-university-theme/includes/search-route.php
+++ wordpress/wp-content/themes/fictional-university-theme/includes/search-route.php
@@ -21,6 +21,8 @@ function universitySearchResults($data) {
     'campuses' => array(),
   );
 
+  $programIDs = array();
+
   while($query->have_posts()) {
     $query->the_post();
 
@@ -43,6 +45,8 @@ function universitySearchResults($data) {
     }
 
     if('program' == get_post_type()) {
+      $programIDs[] = get_the_ID();
+
       array_push($results['programs'], $record);
     }
 
@@ -64,7 +68,66 @@ function universitySearchResults($data) {
 
       array_push($results['events'], $record);
     }
+
+    wp_reset_postdata();
   }
+
+  if(!empty($programIDs)) {
+    $programRelationshipQuery = new WP_Query(array(
+      'post_type' => 'professor',
+
+      /**
+        * 'meta_query' => array(
+        *   'relation' => 'OR',
+        *   array(
+        *     'key' => 'related_programs',
+        *     'compare' => 'LIKE',
+        *     'value' => '"<program_id>"'
+        *   ),
+        *   array(
+        *     'key' => 'related_programs',
+        *     'compare' => 'LIKE',
+        *     'value' => '"<program_id>"'
+        *   )
+        * )
+        */
+      'meta_query' => array_merge(
+        array(
+          'relation' => 'OR'
+        ),
+        array_map(
+          function($pid) {
+            return array(
+              'key' => 'related_programs',
+              'compare' => 'LIKE',
+              'value' => '"' . $pid . '"',
+            );
+          },
+          $programIDs
+        )
+      )
+    ));
+
+    while($programRelationshipQuery->have_posts()) {
+      $programRelationshipQuery->the_post();
+
+      $record = array(
+        'title' => get_the_title(),
+        'permalink' => get_the_permalink()
+      );
+
+      if('professor' == get_post_type()) {
+        $record['image'] = get_the_post_thumbnail_url(0, 'professorLandscape');
+
+        array_push($results['professors'], $record);
+      }
+
+      wp_reset_postdata();
+    }
+  }
+
+  // Deduplicate results
+  $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
 
   return $results;
 }
