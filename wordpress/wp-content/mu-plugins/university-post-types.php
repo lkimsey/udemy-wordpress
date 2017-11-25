579bf365c6f8be82f68a7010dd6a86ac84a65242
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-25 05:45:26 +0530

    Use a custom field for program content to exclude them from search query

--- wordpress/wp-content/mu-plugins/university-post-types.php
+++ wordpress/wp-content/mu-plugins/university-post-types.php
@@ -52,7 +52,7 @@ function university_post_types() {
       'all_items' => 'All Programs'
     ),
     'menu_icon' => 'dashicons-carrot',
-    'supports' => array('title', 'editor')
+    'supports' => array('title')
   ));
 
   // Professor Post Type
