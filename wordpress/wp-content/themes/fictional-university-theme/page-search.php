6bdfa7d22fd3cd30529a8a206f09f49b9eb69aa9
Sudaraka Wijesinghe <sudaraka@sudaraka.org>
2017-11-25 15:11:16 +0530

    Render non-JS search result page with matching styles for each post type

--- wordpress/wp-content/themes/fictional-university-theme/page-search.php
+++ wordpress/wp-content/themes/fictional-university-theme/page-search.php
@@ -9,15 +9,7 @@ while(have_posts()) {
   ?>
   <div class="container container--narrow page-section">
 
-    <div class="generic-content">
-      <form class="search-form" method="get" action="<?php echo esc_url(site_url('/')); ?>">
-        <label class="headline headline--medium"for="s">Perform a New Search:</label>
-        <div class="search-form-row">
-          <input type="search" class="s" name="s" id="s" placeholder="What are you looking for?" />
-          <input type="submit" class="search-submit" value="Search" />
-        </div>
-      </form>
-    </div>
+    <div class="generic-content"><?php get_search_form();?></div>
 
   </div>
   <?php
